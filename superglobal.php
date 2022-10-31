c3e0e8fca37a9072f159bbe39327b6e741b47449
eexxll <117053982+eexxll@users.noreply.github.com>
2022-10-31 19:06:17 +0700

    Add files via upload

---
+++ superglobal.php
@@ -0,0 +1,15 @@
+
+    <?php echo 
+
+      $nama = "Sekolah Koding";
+      $nama = "Bermain Koding";
+      echo "selamat datang di $nama <br>";
+
+      $angka = 1000;
+      $angka2= 3;
+      $angka3= 2;
+
+      $angka *= $angka2;
+      echo $angka;
+ 
+    ?> 
